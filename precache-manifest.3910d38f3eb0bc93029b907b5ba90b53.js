332169858851529b440c526de7acd02913fff4b6
mohanjaga <mj168jayam@gmail.com>
2018-12-07 11:42:48 +0530

    Updates

---
+++ precache-manifest.3910d38f3eb0bc93029b907b5ba90b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e808fd645667c8b36a",
+    "url": "/mohan168/contactmanager/static/js/runtime~main.32e808fd.js"
+  },
+  {
+    "revision": "434e95310a8774f457c4",
+    "url": "/mohan168/contactmanager/static/js/main.434e9531.chunk.js"
+  },
+  {
+    "revision": "ab7ba9f22c6c6561534e",
+    "url": "/mohan168/contactmanager/static/js/1.ab7ba9f2.chunk.js"
+  },
+  {
+    "revision": "ab7ba9f22c6c6561534e",
+    "url": "/mohan168/contactmanager/static/css/1.d16d15a2.chunk.css"
+  },
+  {
+    "revision": "00cafe07928191f407e7d74a38cd4002",
+    "url": "/mohan168/contactmanager/index.html"
+  }
+];
